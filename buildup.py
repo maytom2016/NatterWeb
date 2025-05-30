050aed0ad98e9ac4e5939a60724f23013378d79f
maytom2016 <qwerty448qwe@163.com>
2025-05-30 20:15:51 +0800

    1、buildup.py更新

--- buildup.py
+++ buildup.py
@@ -1,35 +1,38 @@
-import subprocess
-import platform
 import sys
-import codecs
-import os
+sys.path.append('./venv/Thirdparty')
+import venv.Thirdparty.natter as natter
+module = __import__("natter-check")
+# module_name =
+# module_name ="natter"
+# sys.argv = ["","--v"]
+# with open("./venv/Thirdparty/natter.py") as f:
+#     exec(f.read())
+# original_argv = sys.argv.copy()
+check=False
+nt=False
+for arg in sys.argv[1:]:
+    if 'natter-check' in arg:
+        sys.argv.remove(arg)
+        check=True
+    if 'natter.py' in arg:
+        sys.argv.remove(arg)
+        nt = True
+if(check):
+    module.main()
+if(nt):
+    natter.main()
 
 
-sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
-PLATFORM = platform.system()
-ARCHITECTURE = platform.machine()
-# 针对i386架构，由于AMD64架构的宿主机下，i386容器不会通过QEMU全虚拟化，因此会错误地获取到amd64架构
-# 此修正仅适用于宿主机为AMD64架构的情况
-print(platform.architecture()[0])
-if (ARCHITECTURE == "x86_64") & (platform.architecture()[0] == "32bit"):
-    ARCHITECTURE = "i386"
-APP_NAME = "lrcapi"
-APP_VERSION = "1.0.0"
-PACK_NAME = f"{APP_NAME}-{APP_VERSION}-{PLATFORM}-{ARCHITECTURE}{'.exe' if PLATFORM == 'Windows' else ''}"
-
-# 打包
-def generate_add_data_options(root_dir):
-    options = []
-    for root, dirs, files in os.walk(root_dir):
-        if files:
-            # 将目录路径转换为 PyInstaller 可接受的格式
-            formatted_path = root.replace("\\", "/")
-            separator = ";" if PLATFORM == "Windows" else ":"
-            options.append(f'--add-data "{formatted_path}/*{separator}{formatted_path}/"')
-    return " ".join(options)
+# elif(sys.argv.__contains__('./venv/Thirdparty/natter.py')):
+#     sys.argv.remove("./venv/Thirdparty/natter.py")
+# print(sys.argv)
+#
+# 恢复原始命令行参数（可选）
+# sys.argv = original_argv
 
 
-options = generate_add_data_options("/")
-command = f"pyinstaller -F -i logo.png {options} app.py -n {PACK_NAME}"
-subprocess.run(command, shell=True)
+
+
+
+
